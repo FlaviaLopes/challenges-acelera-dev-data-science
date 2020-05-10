b5225daa3be7d75c27f3e6c8badc736cae429be6
Flávia <flavithalopes@hotmail.com>
2020-05-10 18:04:52 -0300

    converte retorno para bool

--- quinto_modulo/main.py
+++ quinto_modulo/main.py
@@ -25,12 +25,8 @@ import seaborn as sns
 
 
 #%matplotlib inline
-
 from IPython.core.pylabtools import figsize
-
-
 figsize(12, 8)
-
 sns.set()
 
 
@@ -83,7 +79,7 @@ def get_sample(df, col_name, n=100, seed=42):
 df = athletes.copy()
 
 
-# In[27]:
+# In[6]:
 
 
 df.head()
@@ -108,7 +104,7 @@ sample_height = get_sample(athletes, 'height', n=3000)
 def q1():
     alpha = 0.05    
     stat, p = sct.shapiro(sample_height)
-    return True if p > alpha else False
+    return bool(p > alpha)
 q1()
 
 
@@ -136,13 +132,13 @@ sm.qqplot(sample_height, fit=True, line="45");
 # 
 # Repita o mesmo procedimento acima, mas agora utilizando o teste de normalidade de Jarque-Bera através da função `scipy.stats.jarque_bera()`. Agora podemos afirmar que as alturas são normalmente distribuídas (ao nível de significância de 5%)? Responda com um boolean (`True` ou `False`).
 
-# In[12]:
+# In[11]:
 
 
 def q2():
     alpha = 0.05    
     stat, p = sct.jarque_bera(sample_height)
-    return True if p > alpha else False
+    return bool(p > alpha)
 q2()
 
 
@@ -154,19 +150,19 @@ q2()
 # 
 # Considerando agora uma amostra de tamanho 3000 da coluna `weight` obtida com a função `get_sample()`. Faça o teste de normalidade de D'Agostino-Pearson utilizando a função `scipy.stats.normaltest()`. Podemos afirmar que os pesos vêm de uma distribuição normal ao nível de significância de 5%? Responda com um boolean (`True` ou `False`).
 
-# In[13]:
+# In[12]:
 
 
 sample_weight = get_sample(athletes, 'weight', n=3000)
 
 
-# In[14]:
+# In[13]:
 
 
 def q3():
     alpha = 0.05    
     stat, p = sct.normaltest(sample_height)
-    return True if p > alpha else False   
+    return bool(p > alpha) 
 q3()
 
 
@@ -175,14 +171,14 @@ q3()
 # * Plote o histograma dessa variável (com, por exemplo, `bins=25`). A forma do gráfico e o resultado do teste são condizentes? Por que?
 # * Um _box plot_ também poderia ajudar a entender a resposta.
 
-# In[15]:
+# In[14]:
 
 
 plt.hist(sample_weight, bins=25)
 plt.show()
 
 
-# In[16]:
+# In[15]:
 
 
 sns.boxplot(x=sample_weight)
@@ -192,7 +188,7 @@ sns.boxplot(x=sample_weight)
 # 
 # Realize uma transformação logarítmica em na amostra de `weight` da questão 3 e repita o mesmo procedimento. Podemos afirmar a normalidade da variável transformada ao nível de significância de 5%? Responda com um boolean (`True` ou `False`).
 
-# In[82]:
+# In[16]:
 
 
 import numpy as np
@@ -201,7 +197,7 @@ sample_weight_log = np.log(sample_weight)
 def q4(): 
     alpha = 0.05    
     stat, p = sct.normaltest(sample_weight_log)
-    return True if p > alpha else False  
+    return bool(p > alpha)
 q4()
 
 
@@ -210,7 +206,7 @@ q4()
 # * Plote o histograma dessa variável (com, por exemplo, `bins=25`). A forma do gráfico e o resultado do teste são condizentes? Por que?
 # * Você esperava um resultado diferente agora?
 
-# In[18]:
+# In[17]:
 
 
 plt.hist(sample_weight_log, bins=25)
@@ -223,20 +219,17 @@ plt.show()
 # 
 # Obtenha todos atletas brasileiros, norte-americanos e canadenses em `DataFrame`s chamados `bra`, `usa` e `can`,respectivamente. Realize um teste de hipóteses para comparação das médias das alturas (`height`) para amostras independentes e variâncias diferentes com a função `scipy.stats.ttest_ind()` entre `bra` e `usa`. Podemos afirmar que as médias são estatisticamente iguais? Responda com um boolean (`True` ou `False`).
 
-# In[108]:
+# In[18]:
 
 
-bra_usa_can = athletes.loc[
-    (athletes['nationality'] == 'BRA') | 
-    (athletes['nationality'] == 'USA') | 
-    (athletes['nationality'] == 'CAN'), :].copy()
+bra = athletes.loc[athletes['nationality'] == 'BRA', 'height'].copy()
+usa = athletes.loc[athletes['nationality'] == 'USA', 'height'].copy()
+can = athletes.loc[athletes['nationality'] == 'CAN', 'height'].copy()
 
 def q5():
-    alpha = 0.05    
-    bra = bra_usa_can.loc[bra_usa_can['nationality'] == 'BRA', 'height'].copy()
-    usa = bra_usa_can.loc[bra_usa_can['nationality'] == 'USA', 'height'].copy()    
+    alpha = 0.05       
     stat, p = sct.ttest_ind(bra, usa, equal_var=False, nan_policy='omit')
-    return True if p > alpha else False    
+    return bool(p > alpha) 
 q5()
 
 
@@ -244,15 +237,13 @@ q5()
 # 
 # Repita o procedimento da questão 5, mas agora entre as alturas de `bra` e `can`. Podemos afimar agora que as médias são estatisticamente iguais? Reponda com um boolean (`True` ou `False`).
 
-# In[107]:
+# In[19]:
 
 
 def q6():
-    alpha = 0.05    
-    bra = bra_usa_can.loc[bra_usa_can['nationality'] == 'BRA', 'height'].copy()
-    can = bra_usa_can.loc[bra_usa_can['nationality'] == 'CAN', 'height'].copy()    
+    alpha = 0.05        
     stat, p = sct.ttest_ind(bra, can, equal_var=False, nan_policy='omit')
-    return True if p > alpha else False  
+    return bool(p > alpha)
 q6()
 
 
@@ -260,12 +251,10 @@ q6()
 # 
 # Repita o procedimento da questão 6, mas agora entre as alturas de `usa` e `can`. Qual o valor do p-valor retornado? Responda como um único escalar arredondado para oito casas decimais.
 
-# In[106]:
+# In[20]:
 
 
-def q7():      
-    usa = bra_usa_can.loc[bra_usa_can['nationality'] == 'USA', 'height'].copy()
-    can = bra_usa_can.loc[bra_usa_can['nationality'] == 'CAN', 'height'].copy()    
+def q7():        
     stat, p = sct.ttest_ind(usa, can, equal_var=False, nan_policy='omit')
     return float(round(p, 8))
 q7()
